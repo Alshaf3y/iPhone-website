daeec6c05e8f90b0085c923458d2e11141689413
Alshaf3y <a.shaf3y93@gmail.com>
2022-09-08 11:41:03 +0200

    Iphone 13 pro max website

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let bodyColor = document.querySelector('body')
+function phones(phone){
+    img.src = phone;
+};
+
+function colors (color){
+    bodyColor.style.background = color;
+
+}
